1f7c37661f7f1c90e956936288c72bb78fc3fdac
north-tower <muneshmyke@gmail.com>
2025-11-30 09:08:19 +0300

    bg

--- config/database.js
+++ config/database.js
@@ -6,7 +6,7 @@ const sequelize = new Sequelize(
   process.env.DB_USER || 'username',
   process.env.DB_PASSWORD || 'password',
   {
-    host: process.env.DB_HOST || 'localhost',
+    host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
     pool: {
